3e3a6cde959f4ae53c560d34fae0b3e0ff9431c3
dhoiffahmad <dhoiff.ahmad@gmail.com>
2024-01-12 19:25:25 +0700

    standbook tinggal gambar

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     // Load HTML yang ingin dijadikan PDF
     // $html = file_get_contents("registrasi-coba.html");
     
-    $html = file_get_contents("perjanjian-tibkam.html");
+    $html = file_get_contents("standbook.html");
 
     // Load HTML ke DOMPDF
     $dompdf->loadHtml($html);
