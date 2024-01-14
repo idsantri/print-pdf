268a120195ba1806ca9d2cff0c898b4ae143c8cb
dhoiffahmad <dhoiff.ahmad@gmail.com>
2024-01-14 15:10:37 +0700

    ket-berhenti sudah dikonversi ke pt

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     // Load HTML yang ingin dijadikan PDF
     // $html = file_get_contents("registrasi-coba.html");
     
-    $html = file_get_contents("standbook.html");
+    $html = file_get_contents("ket-berhenti.html");
 
     // Load HTML ke DOMPDF
     $dompdf->loadHtml($html);
