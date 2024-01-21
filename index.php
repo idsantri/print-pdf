d66b82d543e871c8f7189cc0626e54ee000c14f9
dhoiffahmad <dhoiff.ahmad@gmail.com>
2024-01-22 00:19:26 +0700

    KOP KOREKSI-DATA

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     // Load HTML yang ingin dijadikan PDF
     // $html = file_get_contents("registrasi-coba.html");
     
-    $html = file_get_contents("ket-mds.html");
+    $html = file_get_contents("koreksi-data.html");
 
     // Load HTML ke DOMPDF
     $dompdf->loadHtml($html);
