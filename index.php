6cc1dca66b74ded4d0ebc1395ff9e4933b794166
dhoiffahmad <dhoiff.ahmad@gmail.com>
2024-01-22 00:43:46 +0700

    KOP PERJANJIAN TIBKAM

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     // Load HTML yang ingin dijadikan PDF
     // $html = file_get_contents("registrasi-coba.html");
     
-    $html = file_get_contents("perjanjian-mds2.html");
+    $html = file_get_contents("perjanjian-tibkam.html");
 
     // Load HTML ke DOMPDF
     $dompdf->loadHtml($html);
