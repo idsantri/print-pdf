d5d3f7c1f72d2e6e713c624e3d8af4ac38230a08
dhoiffahmad <dhoiff.ahmad@gmail.com>
2024-01-21 17:10:21 +0700

    break

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     // Load HTML yang ingin dijadikan PDF
     // $html = file_get_contents("registrasi-coba.html");
     
-    $html = file_get_contents("pulang.html");
+    $html = file_get_contents("liburan.html");
 
     // Load HTML ke DOMPDF
     $dompdf->loadHtml($html);
