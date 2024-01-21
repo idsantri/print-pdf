a4bdcd53172d604931dca5e7c132d2e91c141198
dhoiffahmad <dhoiff.ahmad@gmail.com>
2024-01-22 01:14:10 +0700

    KOP REKAM JEJAK

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     // Load HTML yang ingin dijadikan PDF
     // $html = file_get_contents("registrasi-coba.html");
     
-    $html = file_get_contents("registrasi.html");
+    $html = file_get_contents("rekam-jejak.html");
 
     // Load HTML ke DOMPDF
     $dompdf->loadHtml($html);
