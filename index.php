b4b55c3725b6ba71977fd0bee255fb21a2c62573
dhoiffahmad <dhoiff.ahmad@gmail.com>
2024-01-26 20:29:16 +0700

    ket-mds 10pt

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     // Load HTML yang ingin dijadikan PDF
     // $html = file_get_contents("registrasi-coba.html");
     
-    $html = file_get_contents("ket-berhenti.html");
+    $html = file_get_contents("ket-mds.html");
 
     // Load HTML ke DOMPDF
     $dompdf->loadHtml($html);
