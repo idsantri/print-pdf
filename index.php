e2ac72e912d9d9013d16597b5bc45efe73c3b683
dhoiffahmad <dhoiff.ahmad@gmail.com>
2024-01-26 20:23:21 +0700

    Keterangan behenti 10pt

--- index.php
+++ index.php
@@ -29,11 +29,11 @@
     // Load HTML yang ingin dijadikan PDF
     // $html = file_get_contents("registrasi-coba.html");
     
-    $html = file_get_contents("absen-musyawarah.html");
+    $html = file_get_contents("ket-berhenti.html");
 
     // Load HTML ke DOMPDF
     $dompdf->loadHtml($html);
-    $dompdf->setPaper('A4', 'landscape');
+    $dompdf->setPaper('A4', 'potrait');
 
     // Render PDF
     $dompdf->render();
