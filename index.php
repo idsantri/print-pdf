24ddf8d6d783f55c3290db33cec1f1e6bf82e010
dhoiffahmad <dhoiff.ahmad@gmail.com>
2024-01-26 16:12:50 +0700

    Boyong 10pt

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     // Load HTML yang ingin dijadikan PDF
     // $html = file_get_contents("registrasi-coba.html");
     
-    $html = file_get_contents("absen-musyawarah.html");
+    $html = file_get_contents("boyong.html");
 
     // Load HTML ke DOMPDF
     $dompdf->loadHtml($html);
