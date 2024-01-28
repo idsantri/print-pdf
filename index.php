d15db2d694b9a98cd6fa1544fedcc8d595f79029
dhoiffahmad <dhoiff.ahmad@gmail.com>
2024-01-28 17:13:30 +0700

    kwitansi sudah ok, break dulu

--- index.php
+++ index.php
@@ -29,11 +29,11 @@
     // Load HTML yang ingin dijadikan PDF
     // $html = file_get_contents("registrasi-coba.html");
     
-    $html = file_get_contents("surat-tugas.html");
+    $html = file_get_contents("kwitansi.html");
 
     // Load HTML ke DOMPDF
     $dompdf->loadHtml($html);
-    $dompdf->setPaper('A4', 'potrait');
+    $dompdf->setPaper('A5', 'landscape');
 
     // Render PDF
     $dompdf->render();
